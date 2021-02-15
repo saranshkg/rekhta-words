51836298862c869e809b0810f81a6b6f359bf023
Saransh Gupta <saranshkg@gmail.com>
2021-02-16 04:07:10 +0530

    Updated script, included trending words and added .gitignore

--- script.py
+++ script.py
@@ -4,13 +4,36 @@ from bs4 import BeautifulSoup as bs
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-url = "https://www.rekhta.org/urdudictionary"
+url = "https://rekhtadictionary.com/"
 
 r = requests.get(url)
 soup = bs(r.text, "html.parser")
 
-word = soup.findAll("div", {"class": "wordContainer"})[0].text.strip("\n")
-meaning = soup.findAll("div", {"class": "engMeaning"})[0].h3.text
+#print(soup)
+
+#Get daily word and its corresponding meaning
+
+daily_word = soup.findAll("div", {"class": "rdDailywordBlok"})[0].find('span').contents[0]
+daily_word_meaning = soup.findAll("div", {"class": "rdDailymeaningBlock"})[0].find('h3').contents[0]
+# print(daily_word)
+# print(daily_word_meaning)
 
 with open(os.path.join(BASE_DIR, "rekhta-words.txt"), "a") as f:
-	f.write(word + " = " + meaning + "\n")
+	f.write(daily_word + " = " + daily_word_meaning + "\n")
+
+
+# Loop over trending words and get their corresponding meanings
+
+trending_word_cards = soup.findAll("div", {"class": "rdtrndWrd"})
+
+# print(len(trending_word_cards))
+
+for i in range(0, len(trending_word_cards)):
+	trending_word = soup.findAll("div", {"class": "rdtrndWrd"})[i].find('h3').contents[0]
+	trending_word_meaning = soup.findAll("p", {"class": "rdSimilarmeaning"})[i].contents[0]
+
+	# print(trending_word)
+	# print(trending_word_meaning)
+
+	with open(os.path.join(BASE_DIR, "rekhta-words.txt"), "a") as f:
+		f.write(trending_word + " = " + trending_word_meaning + "\n")
