cc496ee1bf4fed8fdc5def4f13b42ffea68ed27c
Saransh Gupta <saranshkg@gmail.com>
2021-02-16 04:37:36 +0530

    Updated batch file

--- run_script_daily.bat
+++ run_script_daily.bat
@@ -1,3 +1,13 @@
-@echo on
+@echo off
 
-cd /d C:\SWE Projects\rekhta-words\venv\Scripts && activate && cd /d C:\SWE Projects\rekhta-words && python script.py && git add . && git commit -m "Words till Feb 16, 2021" && git push origin master
+setlocal ENABLEDELAYEDEXPANSION
+
+set "mo=%Date:~4,2%"
+if "%mo:~0,1%"=="0" set "mo=%mo:~1%"
+set names=JanFebMarAprMayJunJulAugSepOctNovDec
+set /a "pos = 3 * %mo%" - 3
+set "ti=!names:~%pos%,3!"
+
+set "date=%ti% %Date:~7,2%, %Date:~10,4%"
+
+cd /d C:\SWE Projects\rekhta-words\venv\Scripts && activate && cd /d C:\SWE Projects\rekhta-words && python script.py && git add . && git commit -m "Words till %date%" && git push origin master
