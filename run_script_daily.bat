984bdb21fe20bb0aa61afa5b7bd2c5221bfed754
Saransh Gupta <saranshkg@gmail.com>
2021-02-16 04:24:02 +0530

    Words till Feb 16, 2021

---
+++ run_script_daily.bat
@@ -0,0 +1,3 @@
+@echo on
+
+cd /d C:\SWE Projects\rekhta-words\venv\Scripts && activate && cd /d C:\SWE Projects\rekhta-words && python script.py && git add . && git commit -m "Words till Feb 16, 2021" && git push origin master
